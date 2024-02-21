c62f348824620c8fd27ade16ea5d61f9d685f0b0
Alejandro Ruiz <alejandroruizprieto@alumnos.tuniversformacion.es>
2024-02-21 10:09:04 +0100

    js

--- build/js/app.js
+++ build/js/app.js
@@ -1,1 +1,6 @@
-document.getElementById('cod').innerHTML
+const img = document.createElement('imagenes');
+
+function img(){
+    const nav = document.createElement('nav');
+    let navbar = document.createElement('navegador');
+}
